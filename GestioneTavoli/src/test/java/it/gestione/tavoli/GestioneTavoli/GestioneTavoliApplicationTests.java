d469fe6a2627f0448aabfff42119b4b6d1224e45
GabScognamiglio <gabbo16s@gmail.com>
2024-09-09 19:14:00 +0200

    initial commit

---
+++ GestioneTavoli/src/test/java/it/gestione/tavoli/GestioneTavoli/GestioneTavoliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gestione.tavoli.GestioneTavoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneTavoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
